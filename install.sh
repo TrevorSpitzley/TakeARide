e5ef3e6761f281bc3d158b331cfb6cf06efb70e0
Trevor Spitzley <spitzltr@mail.gvsu.edu>
2021-04-18 11:00:03 -0400

    Added install commands to install.sh file

---
+++ install.sh
@@ -0,0 +1,12 @@
+yarn init -y
+yarn add @vue/cli
+
+npx vue create .
+
+yarn add firebase
+yarn add @firebase/firestore-types
+
+yarn add -D @vue/cli-plugin-router
+yarn add vue-router
+
+npx vue add vuetify
